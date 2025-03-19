6be7688b1fd9629c9b86f4d0ee1469e30a554f5e
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-19 13:04:24 -0400

    delta time handling in main.py

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    Clock = pygame.time.Clock()
+    dt = 0    # Delta time (amount of time between a change)
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = Clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
