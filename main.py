ff4ca5dd7d4eccf4a78421fb0437c9903574eaf3
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-21 20:15:33 -0400

    Implement player and asteroid classes with movement and spawning logic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from constants import *
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -8,17 +11,49 @@ def main():
     Clock = pygame.time.Clock()
     dt = 0    # Delta time (amount of time between a change)
 
+    # Create a sprite group for the player
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Assign static containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    #Create game objects
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroid_field = AsteroidField()  # Create an AsteroidField instance
+
+    # Add player and asteroid field to update group
+    updatable.add(player)
+    drawable.add(player)
+    updatable.add(asteroid_field)
+
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Fill the screen with black
         screen.fill((0,0,0))
+        # Draw the player
+        for i in drawable:
+            i.draw(screen)
+        updatable.update(dt)
+        
+    
+        # Update the display
         pygame.display.flip()
+
         dt = Clock.tick(60) / 1000
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
 
 if __name__ == "__main__":
     main()
